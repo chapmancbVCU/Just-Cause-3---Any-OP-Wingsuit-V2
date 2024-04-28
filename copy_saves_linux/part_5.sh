6ddab1e0dfccf662c796d4ab30d0f0a0ad07ef4b
Chad Chapman <chad.chapman2010@gmail.com>
2024-04-28 17:45:35 -0400

    Renamed directories and added copy scripts for Linux

---
+++ copy_saves_linux/part_5.sh
@@ -0,0 +1,12 @@
+game_dir=225540
+save_dir=76561199157138841
+game_path="/mnt/9bb46bd7-a51c-477a-bde6-285e34cd0135/SteamLibrary/steamapps/compatdata/$game_dir/pfx/drive_c/users/steamuser/Documents/Square Enix/Just Cause 3/Saves/$save_dir/"
+
+save_path="/home/chadchapman/Documents/LiveSplit_Files/Just-Cause-3-Any-Percent-OP-Wingsuit-V2/JC3_Saves/Part_5_Begin_Act_III_Missions/"
+
+cd "$save_path"
+cp * "$game_path"
+
+echo -e " \n\n\n\n"
+
+ls "$game_path"
